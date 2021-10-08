28286bdbb262a9bbe2b2796308b85e6cd450af28
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-10-08 10:03:30 -0700

    s

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+require('dotenv').config()
+
+const server = require('./api/server');
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => console.log(`*** listening on port: ${port} ***`))
